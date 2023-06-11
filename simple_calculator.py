e03b9927474c86eb571cb9b2fac22ec606fb255a
Ömer Faruk Özgör <omerfarukozgor9@gmail.com>
2023-06-11 11:33:02 +0300

    Add 'multiply' function to multiply two numbers

--- simple_calculator.py
+++ simple_calculator.py
@@ -7,3 +7,6 @@ def add(number1 : int, number2 : int):
 
 def deduct(number1 : int, number2 : int):
     return number1 - number2
+
+def multiply(number1 : int, number2 : int):
+    return number1 * number2
