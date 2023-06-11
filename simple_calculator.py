d8c28bc498bdb71259a80e4a642a92dd51ac4568
Ömer Faruk Özgör <omerfarukozgor9@gmail.com>
2023-06-11 12:02:04 +0300

    Change parameters of 'add' and 'multiply' functions to variableargs

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,14 +2,18 @@
 
 # A simple calculator
 
-def add(number1 : int, number2 : int):
-    return number1 + number2
+def add(*numbers):
+    return sum(numbers)
 
 def deduct(number1 : int, number2 : int):
     return number1 - number2
 
-def multiply(number1 : int, number2 : int):
-    return number1 * number2
+def multiply(*numbers):
+    result = 1
+    for number in numbers:
+        result *= number
+    return result
 
 def divide(number1 : int, number2 : int):
     return number1 / number2
+
